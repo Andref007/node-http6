452227ac07b587777daf62eb9dd497a328e441ed
Andref007 <andrefps007@hotmail.com>
2022-10-18 20:49:38 -0300

    aprendendo metodo post

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express')
 
 const app = express()
 
-const hostname = '192.168.3.26'
+const hostname = 'localhost'
 const port = 5000
 
 const books = [
-    {_id: 1, title: "O dia do Coringa", autor: "Jostein Gaarden", favorito: true},
-    {_id: 2, title: "O mundo de Sophia", autor: "Jostein Gaarden", favorito: true},
-    {_id: 3, title: "A Casa", autor: "Raquel de Queiroz", favorito: false}
+    { _id: 1, title: "O dia do Coringa", autor: "Jostein Gaarden", favorito: true },
+    { _id: 2, title: "O mundo de Sophia", autor: "Jostein Gaarden", favorito: true },
+    { _id: 3, title: "A Casa", autor: "Raquel de Queiroz", favorito: false }
 ]
 
 
@@ -24,7 +24,7 @@ app.use(express.json())
 
 //uri
 app.get('/', (req, res) => {
-    res.json({ nome: 'André Felipe'})
+    res.json({ nome: 'André Felipe' })
 })
 
 app.get('/html', (req, res) => {
@@ -33,7 +33,26 @@ app.get('/html', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.send(books.filter(i => i.favorito))
-})  
+})
+
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+
+    res.send(req.body)
+
+})
+
+app.get('/books/:id', (req, res) => {
+    const book = books.find((book) => book._id == req.params.id)
+
+
+    if (book)
+        res.send(book)
+
+    else
+        res.status(404)
+})
 
 
 app.listen(port, hostname, () => console.log(`http://${hostname}:${port}`))
